a2c742d336f76d6ada04e230eab8331b26af2407
Reeeix <161616990+Reeeix@users.noreply.github.com>
2026-03-08 15:42:36 +0100

    Update API_URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api"
+const API_URL = "https://movies-explorer-rtc.onrender.com/api"
 
 export const request = async (endpoint, method = "GET", body = null, token = null) => {
   const headers = { "Content-Type": "application/json" }
